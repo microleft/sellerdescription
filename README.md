84dfc9eab5d30882db4f1849b7f7a403614ac11a
microleft <microleft@163.com>
2019-06-09 23:48:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# 淘宝卖家评价详情页
+![TamperMonkey v4.8](https://img.shields.io/badge/tamperMonkey-v4.8-brightgreen.svg) ![Chrome x64 v74.0](https://img.shields.io/badge/chrome%20x64-v73.0-brightgreen.svg) 
+
+👉 本脚本适用于Tampermonkey for Chrome，其他浏览器没有测试。
+## 为什么有这个脚本
+淘宝因产品调整需要，从2019年4月18日起，卖家店铺档案页面将不再展示
+
+[怎样看待淘宝将不再展示卖家店铺档案页面？](https://www.zhihu.com/question/319211686)
+
+**其中有人写了个Chrome扩展。因为不想安装扩展，于是有个这个脚本。**
---
+++ sellerdescription.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         淘宝详情页显示
+// @namespace    https://github.com/microleft/sellerdescription
+// @icon         https://www.taobao.com/favicon.ico
+// @version      0.1
+// @description  禁止淘宝详情页跳转
+// @author       mic
+// @match        https://rate.taobao.com/*
+// @grant        none
+// @run-at       document-body
+// ==/UserScript==
+
+(function() {
+    var body = document.body;
+    var config = {childList: true, subtree: true };
+    var callback = function(mutationsList) {
+        for(var mutation of mutationsList) {
+            if (mutation.type == 'childList') {
+                if ( mutation.target.tagName == 'SCRIPT' &&  mutation.target.innerHTML.indexOf("window.location.href")>0) {
+                    mutation.target.innerHTML= '';
+                }
+
+                if (mutation.target.tagName== 'SCRIPT' && mutation.target.innerHTML.indexOf("addEventListener")>0) {
+                    mutation.target.innerHTML= '';
+                    observer.disconnect();
+                }
+            }
+        }
+    };
+    var observer = new MutationObserver(callback);
+    observer.observe(body, config);
+})();
