ce1be9d73140c16b07ea006dfc1eab788cf9e1cc
microleft <microleft@163.com>
2020-07-21 02:59:17 +0800

    Bump version to 0.2

--- sellerdescription.user.js
+++ sellerdescription.user.js
@@ -2,7 +2,7 @@
 // @name         淘宝详情页显示
 // @namespace    https://github.com/microleft/sellerdescription
 // @icon         https://www.taobao.com/favicon.ico
-// @version      0.1
+// @version      0.2
 // @description  禁止淘宝详情页跳转
 // @author       mic
 // @match        https://rate.taobao.com/*
