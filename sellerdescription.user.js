1334cb843dc27d14c7b9b831998b2fe0e1efacf1
microleft <microleft@163.com>
2020-12-08 02:15:28 +0800

    reset to 1.0

--- sellerdescription.user.js
+++ sellerdescription.user.js
@@ -2,7 +2,7 @@
 // @name         淘宝详情页显示
 // @namespace    https://github.com/microleft/sellerdescription
 // @icon         https://www.taobao.com/favicon.ico
-// @version      0.2
+// @version      0.3
 // @description  禁止淘宝详情页跳转
 // @author       mic
 // @match        https://rate.taobao.com/*
@@ -11,6 +11,21 @@
 // ==/UserScript==
 
 (function () {
-    Object.freeze(document.location);
-    window.alert = console.log;
+    var config = { childList: true, subtree: true };
+    var callback = function (mutationsList) {
+        for (var mutation of mutationsList) {
+            if (mutation.type == 'childList') {
+                if (mutation.target.tagName == 'SCRIPT' && mutation.target.innerHTML.indexOf("window.location.href") >= 0) {
+                    mutation.target.innerHTML = '';
+                }
+
+                if (mutation.target.tagName == 'SCRIPT' && mutation.target.innerHTML.indexOf("addEventListener") >= 0) {
+                    mutation.target.innerHTML = '';
+                    observer.disconnect();
+                }
+            }
+        }
+    };
+    var observer = new MutationObserver(callback);
+    observer.observe(document.body, config);
 })();
